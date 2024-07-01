085a8a50ac7723c14e11bb312de09c68328c5180
Anup Gupta <anup.gupta@nonstopio.com>
2024-07-01 19:12:13 +0530

    Injected upload dependency

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,12 @@
 import express from 'express';
 import { port } from '../server';
 import { fileController } from './controllers/file.controller';
+import { s3Uploader } from './uploaders/s3.uploader';
 
 const app = express();
 
 // Simulating file upload service
-app.get('/file-upload', fileController.upload);
+app.get('/file-upload', (req, res) => fileController.upload(req, res, { uploader: s3Uploader }));
 
 app.get('/', (req, res) => {
     res.json({ message: `Please go to http://localhost:${port}/file-upload` });
--- src/controllers/file.controller.ts
+++ src/controllers/file.controller.ts
@@ -1,12 +1,20 @@
 import { Request, Response } from "express";
-import { s3Uploader } from "../uploaders/s3.uploader";
-import { cloudinaryUploader } from "../uploaders/cloudinary.uploader";
+// import { s3Uploader } from "../uploaders/s3.uploader";
+// import { cloudinaryUploader } from "../uploaders/cloudinary.uploader";
+
+// gives safety to the code to make sure that the uploader has the upload method
+interface Uploader {
+    upload: (filename: string) => Promise<string>;
+}
 
 export const fileController = {
-    upload: async (req: Request, res: Response) => { 
+    upload: async (req: Request, res: Response, { uploader }: { uploader: Uploader}) => { 
         // upload the file to s3 bucket
-        //await s3Uploader.upload('file.txt'); // S3 bucket is a Dependency
-        await cloudinaryUploader.upload('file.txt'); // Cloudinary is a Dependency
+        // await s3Uploader.upload('file.txt'); // S3 bucket is a Dependency
+        // await cloudinaryUploader.upload('file.txt'); // Cloudinary is a Dependency
+
+        // common uploader // injected when file.controller.upload() is called
+        await uploader.upload('file.txt');
 
         res.json({ message: 'File uploaded successfully' });
     },
