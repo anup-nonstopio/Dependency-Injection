d2db3be25582c885a0236d265649e6eb1b8ec9af
Anup Gupta <anup.gupta@nonstopio.com>
2024-07-01 18:44:18 +0530

    file upload controller setupped

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,15 @@
+// Simulating file upload service to learn dependency injection in TypeScript
 import express from 'express';
+import { port } from '../server';
+import { fileController } from './file.controller';
 
 const app = express();
 
+// Simulating file upload service
+app.get('/file-upload', fileController.upload);
+
 app.get('/', (req, res) => {
-    res.json({ message: 'Hello world!' });
+    res.json({ message: `Please go to http://localhost:${port}/file-upload` });
 });
 
 export default app;
